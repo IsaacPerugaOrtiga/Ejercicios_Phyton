14a3032ea6bf6f13630903d9682691c57f6a6f81
IsaacPerugaOrtiga <isaacperuga03@gmail.com>
2024-01-02 20:24:34 +0100

    Ejercicio 5 finalizado(parte 2)

--- Ejercicios Phyton(Parte 2)/ej5.py
+++ Ejercicios Phyton(Parte 2)/ej5.py
@@ -1,1 +1,24 @@
-"""Hola"""
+"""Ejercicio 5
+Construir un pequeño programa que convierta números binarios en enteros.""" 
+
+def reversed_num(num):
+    str = ''
+    for x in num:
+        str = x + str
+    return str
+
+def converter(num):
+    sum = 0
+    n = 0
+    digit = 0
+    str = reversed_num(num)
+    for x in str:
+        n = int(x)
+        str = ''
+        sum += int(n * 2 ** digit) 
+        digit += 1
+    return sum
+
+
+print(converter('010'))
+
