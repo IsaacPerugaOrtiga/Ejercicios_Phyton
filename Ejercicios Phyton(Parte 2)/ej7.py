4f945fec261468edd66e4b4dd5888e8835d78128
IsaacPerugaOrtiga <isaacperuga03@gmail.com>
2024-01-03 19:10:45 +0100

    Ejercicio 7 terminado

---
+++ Ejercicios Phyton(Parte 2)/ej7.py
@@ -0,0 +1,22 @@
+"""Definir una tupla con 10 edades de personas.
+Imprimir la cantidad de personas con edades superiores a 20.
+Puedes variar el ejercicio para que sea el usuario quien ingrese las edades."""
+
+
+def age_people():
+    
+    mylist = []
+    while len(mylist) in range(10):
+        age = int(input("Ingrese la edad de una persona: "))
+        mylist.append(age)
+    return mylist
+
+def age_greater_twenty():
+    tupla = tuple(age_people())
+    cont = 0
+    for i in tupla:
+        if i > 20:
+            cont += 1
+    return "Hay " + str(cont) + " persona/s que tienen mas de 20 años."
+
+print(age_greater_twenty())
