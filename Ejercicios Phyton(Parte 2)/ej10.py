5338ee3c799095ee56a3ea8c6dee15d4ee052bdb
IsaacPerugaOrtiga <isaacperuga03@gmail.com>
2024-01-22 22:29:35 +0100

    Ejercicio 10 terminado

--- Ejercicios Phyton(Parte 2)/ej10.py
+++ Ejercicios Phyton(Parte 2)/ej10.py
@@ -1,3 +1,18 @@
 """Ejercicio 10
 Escriba una función es_bisiesto() que determine si un año determinado es un año
-bisiesto.Un año bisiesto es divisible por 4, pero no por 100. También es divisible por 400"""
+bisiesto.Un año bisiesto es divisible por 4, pero no por 100. Tampoco es divisible por 400"""
+
+def es_bisiesto():
+    year = int(input("¿Que año quieres comprobar que es bisiesto?"))
+    if year % 4 == 0:
+        if year % 100 != 0:
+            if year % 400 != 0:
+                return "El año "+str(year)+" es bisiesto."
+            else:
+                return "El año "+str(year)+" no es bisiesto. Es divisble por 400."
+        else:
+            return  "El año "+str(year)+" no es bisiesto.Es divisble por 100."
+    else:
+        return "El año "+str(year)+" no es bisiesto. No es divisble por 4."
+    
+print(es_bisiesto())
