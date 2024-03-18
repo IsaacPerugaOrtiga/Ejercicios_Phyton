62e046272597789414d24be47f96602895b2c75c
IsaacPerugaOrtiga <isaacperuga03@gmail.com>
2024-03-18 21:39:34 +0100

    Añadir funcionalidad al juego de MasterMind

--- Ejercicios Python(Parte 3)/ej2.py
+++ Ejercicios Python(Parte 3)/ej2.py
@@ -9,13 +9,37 @@ coincide el valor y la posición)."""
 import random
 
 def numbersMasterMindGame():
-    number = int(input("Dame un numero del 2 al 9: "))
-    n = ""
-    if number >= 2 or number <= 9:
-        for it in range(number):
-            it = random.randint(0,9)
-            n += str(it)
-    else:
-       print("El siguiente numero no esta en el rango de 2 a 9.") 
+    try:
+        number = int(input("Dame un numero del 2 al 9: "))
+        n = ""
+        if number >= 2 or number <= 9:
+            for it in range(number):
+                it = random.randint(0,9)
+                n += str(it)
+            return n
+        else:
+            print("El siguiente numero no esta en el rango de 2 a 9.") 
+    except ValueError:
+        print("El campo introducido no es un número")
 
-print(numbersMasterMindGame())
+def startGame():
+    str2 = numbersMasterMindGame()
+    str = input("Introduce la serie de numeros para adivinar el numero secreto")
+    allChecked = False
+    while allChecked:
+        for i in str2:
+            for x in str:
+                if i == x and str2.index(i) == str.index(x):
+                    print("Has acertado un numero y en su posicion")
+                elif i == x and str2.index(i) != str.index(x):
+                    print("Has acertado un número pero no su posición")
+                else: 
+                    print("No has acertado nada")
+        if str == str2:
+            print("Has acertado el número. El juego ha terminado")
+            allChecked = True
+
+    
+    
+
+startGame()
